228f421a20fea47cf9175ce3c0c043b3ddb8cab6
Obed N Munoz <obed.n.munoz@gmail.com>
2021-10-13 21:53:59 -0500

    Update template
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/obedmr/chordiebook-ui
+
+go 1.17
+
+require github.com/aws/aws-sdk-go v1.41.2
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"bytes"
+	"encoding/xml"
 	"fmt"
 	"html/template"
 	"log"
 	"os"
 	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 type Song struct {
 	Name      string
+	Themes    []string
 	ChordsURL string
 	LyricsURL string
 	OpenLPURL string
@@ -28,6 +32,24 @@ func checkError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getTheme(svc *s3.S3, bucket string, obj *s3.Object) []string {
+	rawObject, _ := svc.GetObject(
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    obj.Key,
+		})
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(rawObject.Body)
+	bytesBuff := buf.Bytes()
+
+	q := struct {
+		XMLName xml.Name `xml:"song"`
+		Themes  []string `xml:"properties>themes>theme"`
+	}{}
+	xml.Unmarshal(bytesBuff, &q)
+	return q.Themes
 }
 
 func main() {
@@ -47,8 +69,10 @@ func main() {
 		Bucket: &bucket,
 	}, func(p *s3.ListObjectsOutput, last bool) (shouldContinue bool) {
 		for _, obj := range p.Contents {
-			if strings.HasPrefix(*obj.Key, ChordsPath) {
-				name := strings.Replace(*obj.Key, ChordsPath, "", -1)
+			if strings.HasPrefix(*obj.Key, OpenLPPath) {
+				//fmt.Printf("%#v", obj)
+				themes := getTheme(svc, bucket, obj)
+				name := strings.Replace(*obj.Key, OpenLPPath, "", -1)
 				chordsURL := urlPrefix + ChordsPath + name
 				lyricsURL := urlPrefix + LyricsPath + name
 				openLPURL := urlPrefix + OpenLPPath + name
@@ -56,7 +80,7 @@ func main() {
 				songName := strings.Join(strings.Split(name, "_"), " ")
 				songName = strings.Split(songName, ".")[0]
 				songName = strings.Title(songName)
-				songs = append(songs, Song{songName, chordsURL, lyricsURL, openLPURL})
+				songs = append(songs, Song{songName, themes, chordsURL, lyricsURL, openLPURL})
 			}
 		}
 		return true
@@ -66,7 +90,7 @@ func main() {
 	}
 
 	// HTML Templating
-	t, err := template.New("webpage").Parse(TPL)
+	t, err := template.ParseFiles("template.html")
 	checkError(err)
 
 	data := struct {
