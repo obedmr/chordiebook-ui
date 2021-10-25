ebc8f46aa57ae3e83e41b2bf8437b721e564b3c9
Obed N Munoz <obed.n.munoz@gmail.com>
2021-10-25 01:18:56 -0500

    Add sorting caps
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function myFunction() {
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+    td = tr[i].getElementsByTagName("td")[1];
     if (td) {
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
@@ -17,4 +17,33 @@ function myFunction() {
       }
     }
   }
+}
+
+function download(){
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    selectedURLs = [];
+    downloadIdx = $('#downloadType input:radio:checked').val();
+    counter = 0;
+
+    for (i = 0; i < tr.length; i++) {
+	if (tr[i].className == "selected"){
+	    url = tr[i].getElementsByTagName("a")[downloadIdx].href;
+	    selectedURLs.push(url);
+	    counter++;
+	}
+    }
+
+    answer = false
+    if (counter > 10) {
+	answer = window.confirm("Are you sure that you cant to open "+counter+" documents?");
+    }
+
+    if (counter <= 10 || answer)
+    selectedURLs.forEach(
+	function(item, index, arr) {
+	    url = arr[index];
+	    window.open(url, "_blank");
+	}
+    )
 }
